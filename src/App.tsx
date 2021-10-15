47b5d2298ffb7933efc46d73eec9054e7ee776e1
Rick Chi <wenzhongchi@gmail.com>
2021-10-14 23:08:48 -0400

    code challenge

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+/* 
+  Implement a page which makes a get request to https://reqres.in/ to get a list of users and display them when page loads
+  feel free to accomplish this goal however you wish, within the context of react.
+  feel free to use any library you like
+
+  api.js file has already been created for you. Feel free to use it or not.
+
+
+  1:  Add a button for each user to make a delete request to delete that user.
+      Update the displayed users excluding the deleted user.
+
+  2: Make a filter box to filter the displayed users by name.
+*/
+
+import React from "react";
+
+import "./styles.css";
+
+const App: React.FC = () => {
+  return <div className="App">React Code Challenge</div>;
+};
+
+export default App;
---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+/*
+  Go to https://reqres.in/ for documentation on this api.
+  
+  If you haven't used axios, documentation here: https://github.com/axios/axios
+  OR use any method / library you're comfortable with to perform the request(s).
+
+  Feel free to change whatever you like to accomplish the goal
+*/
+
+import axios from "axios";
+
+const baseUrl = "https://reqres.in/";
+
+export const getUsers = () => {};
+
+export const deleteUser = () => {};
